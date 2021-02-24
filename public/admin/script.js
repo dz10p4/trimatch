27f717cd75f5f1ee3870e8b9cad0d73e9b7111a8
daszek0 <daszek2003@gmail.com>
2021-02-24 23:44:49 +0100

    stachu

---
+++ public/admin/script.js
@@ -0,0 +1,4 @@
+function lecimy() {
+    document.querySelector("#forma").innerHTML = '<iframe width="0" height="0" src="https://www.youtube-nocookie.com/embed/WpSSDEzvpM8?controls=0&autoplay=1&disablekb=1&loop=1&modestbranding=1&playsinline=1&rel=0&showinfo=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+    document.getElementById("forma").style.display = 'none';
+}
---
+++ public/admin/show.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<head>
+    <title>Admin</title>
+    <link rel="icon" type="image/png" href="https://trimatch.date/mailzwr/logo.png">
+    <link href="style.css" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="viewport" content="height=device-height, initial-scale=1" />
+    <script src="script.js"></script>
+</head>
+
+<body>
+
+    <img class="main" src="logoimg.gif">
+
+    <div id="forma" style="display: table;">
+        <div style="display: table-row;">
+            <div style="display: table-cell;vertical-align: middle; text-align: center;">
+                <img src="https://trimatch.date/mailzwr/logo.png" style="width:70px; height:70px; margin-bottom: 30px;"><br>
+                <p>PANEL ADMINISTRACYJNY TRIMATCH</p>
+                <input type="text" placeholder="Login"><br>
+                <input type="password" placeholder="Hasło"><br>
+                <input type="button" value="Zaloguj" onclick="lecimy()">
+            </div>
+        </div>
+
+
+
+    </div>
+
+
+</body>
+
+</html>
