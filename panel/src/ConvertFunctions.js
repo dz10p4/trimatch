b81b21efebb5733d8eaf5bc1b3e007e63288cfbe
daszek0 <daszek2003@gmail.com>
2022-02-25 22:51:05 +0100

    funkcja do liczenia punktów

--- panel/src/ConvertFunctions.js
+++ panel/src/ConvertFunctions.js
@@ -41,7 +41,60 @@ function ConvertFunctions() {
 						lookingFor.nonbinary = true;
 						break;
 				}
-				db.collection("users").doc(person.id).set({ fullName: person.data().name.trim() + " " + person.data().surname.trim(), gender: gender, lookingFor: lookingFor, matched: false, partners: [] }, { merge: true });
+				let fromTrójka = true;
+				if (person.data().profile == "spoza Trójki")
+					fromTrójka = false;
+				db.collection("users").doc(person.id).set({ fullName: person.data().name.trim() + " " + person.data().surname.trim(), gender: gender, lookingFor: lookingFor, matched: false, partners: [], isFromTrójka: fromTrójka }, { merge: true });
+			});
+		});
+	}
+
+	function countPoints() {
+		db.collection("users").get().then(function (querySnapshot) {
+			querySnapshot.forEach((person) => {
+				const pointsTab = {};
+				db.collection("users").get().then(function (querySnapshot) {
+					querySnapshot.forEach((partner) => {
+						if (person.id != partner.id) {
+							if (person.data().lookingFor[partner.data().gender] && partner.data().lookingFor[person.data().gender]) {
+								let points = 0;
+								// liczenie punktów
+								// - nie ta sama klasa +6
+								if (person.data().year != partner.data().year || person.data().profile != partner.data().profile || !person.data().isFromTrójka || !partner.data().isFromTrójka) {
+									points += 6;
+								}
+								// - ten sam profil +2
+								if (person.data().profile == partner.data().profile) {
+									points += 2;
+								}
+								// priorytet +4 [2]
+								if (person.data().answers[2] == partner.data().answers[2]) {
+									points += 4;
+								}
+								// wymarzona randka +3 [3]
+								if (person.data().answers[3] == partner.data().answers[3]) {
+									points += 3;
+								}
+								// w towarzystwie rówieśników +3 [4]
+								if (person.data().answers[4] == partner.data().answers[4]) {
+									points += 3;
+								}
+								// czas wolny +2 [5]
+								if (person.data().answers[5] == partner.data().answers[5]) {
+									points += 2;
+								}
+								pointsTab[partner.id] = points;
+							}
+							else {
+								pointsTab[partner.id] = -1;
+							}
+						}
+						else {
+							pointsTab[partner.id] = -1;
+						}
+					});
+					db.collection("users").doc(person.id).set({ points: pointsTab }, { merge: true });
+				});
 			});
 		});
 	}
