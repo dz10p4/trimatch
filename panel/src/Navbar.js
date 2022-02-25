e9427104b8d4196642814b8815fe1aebaadda47e
Jakub Dziopa <jakub.dziopa@gmail.com>
2022-02-25 23:00:12 +0100

    navbar przeniesiony wyżej

--- panel/src/Navbar.js
+++ panel/src/Navbar.js
@@ -11,13 +11,14 @@ function Navbar({handlerFunction, currentState}) {
             <div className="cselect">
                 <select name="selector" className="selector" onChange={handlerFunction} value={currentState}>
                     <option id="s3" value="3">Wyloguj</option>
+                    <option id="s5" value="5">Funkcje</option>
                     <optgroup label="Listy:">
                         <option id="s1" value="1">Wszystkie osoby wpisane</option>
                         <option id="s2" value="2">Osoby dobrane</option>
                     </optgroup>
                     <optgroup label="Dobieranie:">
                         <option id="s4" value="4">Dobieraj</option>
-                        <option id="s5" value="5">Funkcje</option>
+                        
                     </optgroup>
                 </select>
             </div>
